e252386f335a1bdc1b1dc98fb5a930799ad22f20
Jannis Gebauer <ja.geb@me.com>
2018-02-05 16:28:44 +0100

    new release, 0.14

--- changelogs/__init__.py
+++ changelogs/__init__.py
@@ -15,7 +15,7 @@ else:
 
 __author__ = """Jannis Gebauer"""
 __email__ = 'jay@pyup.io'
-__version__ = '0.13.0'
+__version__ = '0.14.0'
 
 
 url_re = re.compile(r"(https?://[^\s<>\"'\x7f-\xff]+)", re.IGNORECASE)
--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ test_requirements = [
 
 setup(
     name='changelogs',
-    version='0.13.0',
+    version='0.14.0',
     description="A changelog finder and parser.",
     long_description=readme + '\n\n' + history,
     author="Jannis Gebauer",
