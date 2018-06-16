f9aa76cea7c1d2d5d25b86a6c54aead8087c7c20
chris48s <chris.shaw480@gmail.com>
2018-06-16 12:35:08 +0100

    add custom changelog parser for django-apiblueprint-view
    https://pypi.org/project/django-apiblueprint-view/

---
+++ changelogs/custom/pypi/django_apiblueprint_view.py
@@ -0,0 +1,13 @@
+import re
+
+def get_urls(releases, **kwargs):
+    return {
+        'https://raw.githubusercontent.com/chris48s/django-apiblueprint-view/master/CHANGELOG.md'
+    }, set()
+
+def get_head(line, releases, **kwargs):
+    for release in releases:
+        pattern = re.compile(r"## :package: [[]?{}[]]?.+".format(release))
+        if pattern.match(line):
+            return release
+    return False
