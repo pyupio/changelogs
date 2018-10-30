95975454db92751d465f4e65876fb94bdf8638f3
sneakypete81 <sneakypete81@gmail.com>
2018-10-30 12:12:58 +0000

    Fix sphinx_rtd_theme changelog URL

--- changelogs/custom/pypi/sphinx_rtd_theme.py
+++ changelogs/custom/pypi/sphinx_rtd_theme.py
@@ -1,4 +1,4 @@
 def get_urls(*args, **kwargs):
     return {
-        'https://raw.githubusercontent.com/rtfd/sphinx_rtd_theme/master/README.rst'
+        'https://raw.githubusercontent.com/rtfd/sphinx_rtd_theme/master/docs/changelog.rst'
     }, set()
